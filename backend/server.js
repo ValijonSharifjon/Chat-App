ce3bf621d4bb53261ebac78480cc4aa8e04c11f9
Valijon <sharifjonovvalijon@gmail.com>
2024-05-23 17:07:27 +0500

    first par completed - backend

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from 'cookie-parser'
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js"
+
+import {connect} from "mongoose";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
+
+
+app.listen(PORT, () => {
+    connectToMongoDB()
+    console.log(`Server Running on port ${PORT}`)
+})
